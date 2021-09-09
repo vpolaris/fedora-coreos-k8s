6f6c134544f6a1c3d33823cbec66e76ac47d4bcb
Olivier Sylva <szfd9g@live.fr>
2021-09-09 15:12:58 +0200

    more helm installation support

--- setup_master.sh
+++ setup_master.sh
@@ -2,6 +2,7 @@
 
 #Initialize script environment
 conf_dir="/root/.k8s-install/config"
+log_file="/tmp/k8s-init.log"
 export c_version="$(rpm -qi cri-o | grep Version | cut  -d':' -f2 |xargs)"
 export k_version="$(echo v"$(rpm -qi kubeadm | grep Version | cut  -d':' -f2 |xargs)")"
 export TOKEN="$(kubeadm token create)"
@@ -46,14 +47,16 @@ kubeadm init phase certs all --config "${conf_dir}/Kubernetes.yaml"
 export SHA="$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //')"
 
 #Install Kubernetes
-kubeadm init --config "${conf_dir}/Kubernetes.yaml" --v=3 > /tmp/k8s-init.log
+printf "K8S installtion started.\n "
+kubeadm init --config "${conf_dir}/Kubernetes.yaml" --v=3 > ${log_file}
 if [ '$(grep "Your Kubernetes control-plane has initialized successfully!" /tmp/k8s-init.log)'!="" ]; then
-  printf 'installation of k8s cluster success\n'
+  printf 'installation of K8S master success\n'
 else
-  printf 'installation of k8s cluster failed\n'
+  printf 'installation of K8S master failed\n'
   exit 1
 fi
 
+printf "Post K8S installtion steps.\n "
 mv "${conf_dir}/Kubernetes.yaml" "${conf_dir}/Kubernetes.yaml.bkp"
 echo 'export KUBECONFIG=/etc/kubernetes/admin.conf' >> ~/.bash_profile
 export KUBECONFIG=/etc/kubernetes/admin.conf
@@ -68,26 +71,42 @@ sudo chown -R core:core /home/core/.kube
 rm /root/.k8s-install/1stage
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
-sleep 90
+sleep 120
 
 #Deploy flannel
+#https://github.com/flannel-io/flannel
+printf "Flannel installtion started.\n "
 kubectl patch node $(hostname) -p '{"spec":{"podCIDR":"10.11.0.0/16"}}'
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml >> ${log_file}
+sleep 120
 
-sleep 90
+#Install Helm
+curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 -o /tmp/get_helm.sh
+chmod 700 /tmp/get_helm.sh
+sh /tmp/get_helm.sh
+HLBIN="/usr/local/bin/helm"
 
 #Deploy Ingress NGINX
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.0/deploy/static/provider/baremetal/deploy.yaml
+#https://kubernetes.github.io/ingress-nginx/deploy/
 
-sleep 90
+printf " Ingress NGINX installtion started.\n "
+${HLBIN} repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+${HLBIN} repo update
+${HLBIN} install ingress-nginx ingress-nginx/ingress-nginx >> ${log_file}
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.0/deploy/static/provider/baremetal/deploy.yaml
+
+POD_NAME=$(kubectl get pods -l app.kubernetes.io/name=ingress-nginx -o jsonpath='{.items[0].metadata.name}')
+kubectl exec -it $POD_NAME -- /nginx-ingress-controller --version
+
+sleep 120
 
 #Deploy MetalLB
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/metallb.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+#https://metallb.universe.tf/
 
-
-cat << EOF | kubectl apply -f -
+printf "MetalLB installtion started.\n "
+MLBCONFIG="${conf_dir}/metallb_values.yaml"
+# cat << EOF | kubectl apply -f -
+cat << EOF > ${MLBCONFIG}
 apiVersion: v1
 kind: ConfigMap
 metadata:
@@ -101,20 +120,29 @@ data:
       addresses:
       - ${NETRANGE}.210-${NETRANGE}.254
 EOF
-      
-sleep 90
+
+${HLBIN} repo add metallb https://metallb.github.io/metallb
+${HLBIN} repo update
+${HLBIN} install metallb metallb/metallb -f ${MLBCONFIG} >> ${log_file}
+# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/namespace.yaml
+# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"      
+mv "${MLBCONFIG}" "${MLBCONFIG}.bkp"
+
+sleep 120
 
 #Install HAProxy
-curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 -o /tmp/get_helm.sh
-chmod 700 /tmp/get_helm.sh
-sh /tmp/get_helm.sh
-/usr/local/bin/helm repo add haproxy-ingress https://haproxy-ingress.github.io/charts
-echo -e "controller:\\n  hostNetwork: true" > "${conf_dir}/haproxy-ingress-values.yaml"
-/usr/local/bin/helm install haproxy-ingress haproxy-ingress/haproxy-ingress\
+
+printf "HAProxy installtion started.\n "
+HACONFIG="${conf_dir}/haproxy-ingress-values.yaml"
+${HLBIN} repo add haproxy-ingress https://haproxy-ingress.github.io/charts
+${HLBIN} repo update
+echo -e "controller:\\n  hostNetwork: true" > ${HACONFIG}
+${HLBIN}install haproxy-ingress haproxy-ingress/haproxy-ingress\
   --create-namespace --namespace ingress-controller\
   --version 1.0.0\
-  -f "${conf_dir}/haproxy-ingress-values.yaml"
-mv  "${conf_dir}/haproxy-ingress-values.yaml"  "${conf_dir}/haproxy-ingress-values.yaml.bkp"
+  -f "${HACONFIG}" >> ${log_file}
+mv "${HACONFIG}"  "${HACONFIG}.bkp"
 
 #Setup network rules
 iptables -P FORWARD ACCEPT
@@ -123,17 +151,21 @@ iptables -A INPUT -p tcp -m tcp --dport 8089 -j ACCEPT
 iptables-save > /etc/sysconfig/iptables
 
 #Setup mDNS web server
+
+printf "Setup mDNS web server.\n "
+SHARE="/var/srv/share"
+mkdir -p ${SHARE}
 podman pull -q  docker.io/pierrezemb/gostatic
-podman run -dt -p 8089:8043 -v /var/srv/share:/srv/http --name goStatic pierrezemb/gostatic
+podman run -dt -p 8089:8043 -v ${SHARE}:/srv/http --name goStatic pierrezemb/gostatic
 curl -sSL https://raw.githubusercontent.com/vpolaris/fedora-coreos-k8s/main/config/http.service -o /etc/avahi/services/http.service
 
+KUBCONFIG="${SHARE}/kubejoin.ini"
+printf "NETDEVICE=${NETDEVICE}\n" > ${KUBCONFIG}
+printf "HOSTNAME=$(hostname -f)\n" >> ${KUBCONFIG}
+printf "IPV4=${IPV4}\n" >> ${KUBCONFIG}
+printf "TOKEN=${TOKEN}\n" >> ${KUBCONFIG}
+printf "SHA=${SHA}\n" >> ${KUBCONFIG}
+chown core:core ${KUBCONFIG}
+chmod 766 ${KUBCONFIG}
 
-printf "NETDEVICE=${NETDEVICE}\n" > /srv/share/kubejoin.ini
-printf "HOSTNAME=$(hostname -f)\n" >> /srv/share/kubejoin.ini
-printf "IPV4=${IPV4}\n" >> /srv/share/kubejoin.ini
-printf "TOKEN=${TOKEN}\n" >> /srv/share/kubejoin.ini
-printf "SHA=${SHA}\n" >> /srv/share/kubejoin.ini
-chown core:core /srv/share/kubejoin.ini
-chmod 766 /srv/share/kubejoin.ini
-
-
+printf "Setup K8S master completed.\n "
